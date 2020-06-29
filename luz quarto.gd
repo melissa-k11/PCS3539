57a8027d3ea5022a9356fcb8a5a9b7e63a065d67
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-29 03:49:09 -0300

    Update luz quarto.gd

--- luz quarto.gd
+++ luz quarto.gd
@@ -1,16 +1,20 @@
 extends OmniLight
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+var sit: int=0
+var player = AudioStreamPlayer.new()
 func _ready():
-	pass # Replace with function body.
+	pass
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input (event):
+	if Input.is_key_pressed(KEY_T):
+		if sit==0:
+			self.add_child(player)
+			player.stream=load("res://luz_on.wav")
+			player.play()
+			show()
+			sit = 1
+		else:
+			player.stream=load("res://luz_off.wav")
+			player.play()
+			hide()
+			sit =0
