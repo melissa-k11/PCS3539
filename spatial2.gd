c7e9ac40d3c9cacf4891dff31d82dcd43a9da1d6
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-29 02:09:41 -0300

    Update spatial2.gd

--- spatial2.gd
+++ spatial2.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-
+var sit: int=0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -10,7 +10,15 @@ extends Spatial
 func _ready():
 	pass # Replace with function body.
 func _input (event):
-	if Input.is_key_pressed(KEY_1):
+	if sit&1: hide()
+	else:
 		show()
-	elif Input.is_key_pressed(KEY_2):
-		hide()
+		sit=0
+	if Input.is_key_pressed(KEY_1):
+		sit = sit+1
+	print(sit)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
