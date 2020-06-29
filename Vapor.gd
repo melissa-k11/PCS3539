0324822695dd3fbe2f10e066105b8e8c24b85247
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-29 03:48:42 -0300

    Create Vapor.gd

---
+++ Vapor.gd
@@ -0,0 +1,24 @@
+extends Particles
+var sit: int=0
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+	
+
+func _input (event):
+	if Input.is_key_pressed(KEY_3):
+		if sit==0:
+			show()
+			sit = 1
+		else:
+			hide()
+			sit =0
+
+
