90eb9235e3988da6ccaa4565570441650e49f461
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-29 03:48:33 -0300

    Update WorldEnvironment.gd

--- WorldEnvironment.gd
+++ WorldEnvironment.gd
@@ -1,5 +1,5 @@
 extends WorldEnvironment
-
+var quarto: int=0
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,8 +9,11 @@ extends WorldEnvironment
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input (event):
+	if Input.is_key_pressed(KEY_1):
+		quarto = quarto+1
+		if quarto==51: $AudioStreamPlayer3D.play()
+		if quarto==52:
+			$AudioStreamPlayer3D.stop()
+			quarto=1
+print(quarto)
