e26407f6584dd95692e36bca5dfed049cb42e9f0
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-29 02:10:13 -0300

    Update OmniLight3.gd

--- OmniLight3.gd
+++ OmniLight3.gd
@@ -1,10 +1,14 @@
 extends OmniLight
-
+var sit: int=0
 func _ready():
 	pass
+	
 
 func _input (event):
-	if Input.is_key_pressed(KEY_U):
-		show()
-	elif Input.is_key_pressed(KEY_Y):
+	if sit&1: show()
+	else:
 		hide()
+		sit=0
+	if Input.is_key_pressed(KEY_Y):
+		sit = sit+1
+	print(sit)
