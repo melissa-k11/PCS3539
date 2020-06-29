7c548d9f4929e31917fa87374d53b712b42b7c10
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-29 02:10:53 -0300

    Update fogo.gd

--- fogo.gd
+++ fogo.gd
@@ -1,13 +1,17 @@
-extends Spatial
 
-
+var sit: int=0
 func _ready():
-	pass 
+	pass
 
+	
 
 func _input (event):
-	if Input.is_key_pressed(KEY_B):
-		show()
-	elif Input.is_key_pressed(KEY_V):
-		hide()
-
+	if Input.is_key_pressed(KEY_9):
+		if sit==0:
+			$AudioStreamPlayer3D.play()
+			show()
+			sit = 1
+		else:
+			hide()
+			sit =0
+	print(sit)
