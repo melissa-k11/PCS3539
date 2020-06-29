f8b2d6c99eb0a5e83f79c2732d6586d470d4f02f
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-29 02:10:36 -0300

    Create luz verde.gd

---
+++ luz verde.gd
@@ -0,0 +1,21 @@
+extends OmniLight
+var sit: int=0
+var player = AudioStreamPlayer.new()
+func _ready():
+	pass
+	
+
+func _input (event):
+	if Input.is_key_pressed(KEY_G):
+		if sit==0:
+			self.add_child(player)
+			player.stream=load("res://luz_on.wav")
+			player.play()
+			show()
+			sit = 1
+		else:
+			player.stream=load("res://luz_off.wav")
+			player.play()
+			hide()
+			sit =0
+	print(sit)
