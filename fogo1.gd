778ae639ce6ba5c40dc6876b2384e44fbd8b8a8e
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-29 02:10:51 -0300

    Create fogo1.gd

---
+++ fogo1.gd
@@ -0,0 +1,18 @@
+extends Spatial
+var sit: int=0
+func _ready():
+	pass
+	
+
+func _input (event):
+	if Input.is_key_pressed(KEY_9):
+		if sit==0:
+			$AudioStreamPlayer3D.play()
+			show()
+			sit = 1
+		else:
+			$AudioStreamPlayer3D.stop()
+			hide()
+			sit =0
+	print(sit)
+
