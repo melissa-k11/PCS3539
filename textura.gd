c2705c6cb0053e07ffa2bc0132b03f77ce6a32da
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-30 04:37:42 -0300

    Update textura.gd

--- textura.gd
+++ textura.gd
@@ -1,24 +1,14 @@
 extends Spatial
-
-var quarto: int=0
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+var sit: int=0
 func _ready():
-	pass # Replace with function body.
-func _input (event):
+	pass
 	
-	if Input.is_key_pressed(KEY_1):
-		quarto = quarto+1
-		if quarto&1:
-			if quarto==51:
-				hide()
-				quarto=0
-			else: show()
-		else: hide()
-		
 
-
+func _input (event):
+	if Input.is_key_pressed(KEY_3):
+		if sit==0:
+			hide()
+			sit = 1
+		else:
+			show()
+			sit =0
