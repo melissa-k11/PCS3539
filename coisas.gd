7103b5abacc2d1e1b46ff704a154723d89a3585f
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-30 04:41:35 -0300

    Update coisas.gd

--- coisas.gd
+++ coisas.gd
@@ -4,7 +4,7 @@ var quarto: int=0
 func _ready():
 	pass # Replace with function body.
 func _input (event):
-	if Input.is_key_pressed(KEY_1):
+	if Input.is_key_pressed(KEY_1) or Input.is_key_pressed(KEY_2) or Input.is_key_pressed(KEY_3) or Input.is_key_pressed(KEY_4) or Input.is_key_pressed(KEY_5) or Input.is_key_pressed(KEY_6) or Input.is_key_pressed(KEY_7) or Input.is_key_pressed(KEY_T) or Input.is_key_pressed(KEY_U) or Input.is_key_pressed(KEY_Y) or Input.is_key_pressed(KEY_G) or Input.is_key_pressed(KEY_H) or Input.is_key_pressed(KEY_J) or Input.is_key_pressed(KEY_V):
 		quarto = quarto+1
 		if quarto==51: 
 			hide()
