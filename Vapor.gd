9989784e3ee5e58fa98d025cc725972655a3cc78
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-30 04:37:33 -0300

    Update Vapor.gd

--- Vapor.gd
+++ Vapor.gd
@@ -1,24 +1,18 @@
 extends Particles
+
 var sit: int=0
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 	
 
 func _input (event):
-	if Input.is_key_pressed(KEY_3):
+	if Input.is_key_pressed(KEY_6):
 		if sit==0:
+			$AudioStreamPlayer3D.play()
 			show()
 			sit = 1
 		else:
+			$AudioStreamPlayer3D.stop()
 			hide()
-			sit =0
-
+			sit = 0
 
