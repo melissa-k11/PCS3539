a232ad168b5c6b4a3b95eece387e5df3b67e20d8
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-30 04:37:55 -0300

    Update SpotLight.gd

--- SpotLight.gd
+++ SpotLight.gd
@@ -1,11 +1,21 @@
 extends SpotLight
 
+var sit: int=0
+var player = AudioStreamPlayer.new()
 func _ready():
 	pass
+	
 
 func _input (event):
-	if Input.is_key_pressed(KEY_M):
-		show()
-	elif Input.is_key_pressed(KEY_N):
-		hide()
-
+	if Input.is_key_pressed(KEY_V):
+		if sit==0:
+			self.add_child(player)
+			player.stream=load("res://luz_on.wav")
+			player.play()
+			show()
+			sit = 1
+		else:
+			player.stream=load("res://luz_off.wav")
+			player.play()
+			hide()
+			sit =0
