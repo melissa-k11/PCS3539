8bbe6a500bc530be86e427943ee98b9c0ef6daf4
melissa-k11 <65129795+melissa-k11@users.noreply.github.com>
2020-06-30 04:37:46 -0300

    Create suzanne.gd

---
+++ suzanne.gd
@@ -0,0 +1,19 @@
+extends MeshInstance
+
+var sit: int=0
+func _ready():
+	pass
+	
+
+func _input (event):
+	if Input.is_key_pressed(KEY_7):
+		if sit==0:
+			$AudioStreamPlayer3D.play()
+			show()
+			sit = 1
+		else:
+			$AudioStreamPlayer3D.stop()
+			hide()
+			sit =0
+
+
